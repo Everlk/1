df25fbd887bb4d12dead114754b757e726786e0b
Ever <108511045+Everlk@users.noreply.github.com>
2023-02-14 18:42:01 +0800

    Update a2dp_vendor_lhdcv3_dec_AOSP12.cc

--- a2dp_vendor_lhdcv3_dec_AOSP12.cc
+++ a2dp_vendor_lhdcv3_dec_AOSP12.cc
@@ -1,5 +1,5 @@
 /******************************************************************************
- *
+ */////
  *  Copyright 2002-2012 Broadcom Corporation
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
